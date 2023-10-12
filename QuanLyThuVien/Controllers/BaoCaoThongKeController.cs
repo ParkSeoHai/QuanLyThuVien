eab7ecd375ad427fb003ef15533ff76fdfc613f9
Hai Nguyen <anhhai282003@gmail.com>
2023-10-12 19:12:51 +0700

    Lam xong chuc nang baocaothongke/ sachmuonnhieunhat

--- QuanLyThuVien/Controllers/BaoCaoThongKeController.cs
+++ QuanLyThuVien/Controllers/BaoCaoThongKeController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLyThuVien.Data;
+using QuanLyThuVien.Models;
 
 namespace QuanLyThuVien.Controllers
 {
     public class BaoCaoThongKeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public BaoCaoThongKeController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<ThongKeSachMuonNhieu> lstSach = new List<ThongKeSachMuonNhieu>();
+            var data = from s in _db.Saches
+                       from tl in _db.TheLoais
+                       from tg in _db.TacGias
+                       from pm in _db.PhieuMuons
+                       from ctpm in _db.CTPhieuMuon
+                       where s.ID_Sach == ctpm.ID_Sach && tl.ID_TheLoai == s.ID_TheLoai
+                       && s.ID_TacGia == tg.ID_TacGia && ctpm.ID_PhieuMuon == pm.ID_PhieuMuon
+                       && ctpm.TrangThai == 1
+                       orderby ctpm.SoLuongMuon descending
+                       select new
+                       {
+                           obj = new ThongKeSachMuonNhieu(s.ID_Sach, s.TenSach, tg.TenTacGia, tl.TenTheLoai, s.NgayNhap, s.GiaTien, ctpm.SoLuongMuon)
+                       };
+            foreach (var item in data)
+            {
+                lstSach.Add(item.obj);
+            }
+
+            return View(lstSach);
         }
     }
 }
---
+++ QuanLyThuVien/Models/ThongKeSachMuonNhieu.cs
@@ -0,0 +1,34 @@
+﻿namespace QuanLyThuVien.Models
+{
+    public class ThongKeSachMuonNhieu
+    {
+        private int idSach;
+        private string tenSach;
+        private string tenTacGia;
+        private string tenTheLoai;
+        private DateTime ngayNhap;
+        private double giaBan;
+        private int soLuongMuon;
+
+        public string TenSach { get => tenSach; set => tenSach = value; }
+        public string TenTacGia { get => tenTacGia; set => tenTacGia = value; }
+        public string TenTheLoai { get => tenTheLoai; set => tenTheLoai = value; }
+        public DateTime NgayNhap { get => ngayNhap; set => ngayNhap = value; }
+        public double GiaBan { get => giaBan; set => giaBan = value; }
+        public int SoLuongMuon { get => soLuongMuon; set => soLuongMuon = value; }
+        public int IdSach { get => idSach; set => idSach = value; }
+
+        public ThongKeSachMuonNhieu() { }
+
+        public ThongKeSachMuonNhieu(int id, string tenSach, string tenTacGia, string tenTheLoai, DateTime ngayNhap, double giaBan, int soLuongMuon)
+        {
+            IdSach = id;
+            TenSach = tenSach;
+            TenTacGia = tenTacGia;
+            TenTheLoai = tenTheLoai;
+            NgayNhap = ngayNhap;
+            GiaBan = giaBan;
+            SoLuongMuon = soLuongMuon;
+        }
+    }
+}
